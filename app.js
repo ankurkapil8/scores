6b4e50ffa62050b505e553f1cb8ab57a2163b0b7
ankur kapil <ankur.kapil@aricent.com>
2020-10-04 13:10:37 +0530

    submit board api

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express();
 const bodyParser = require('body-parser');
 var jwt = require('jsonwebtoken');
 app.set('superSecret', "scores");
+var customSortController = require('./controller/scoreController');
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
@@ -49,7 +50,8 @@ app.post("/create-user", (req, res, next) => {
         });
       } else {
         var payload = { 'name': name, 'imageUrl': imageUrl, 'uniqueId': uniqueId };
-        var token = jwt.sign(payload, app.get('superSecret'), { expiresIn: '2h' });
+        let token = name+"-"+uniqueId+"-"+this.lastID;
+        //var token = jwt.sign(payload, app.get('superSecret'), { expiresIn: '2h' });
         return res.json({
           success: true,
           token: token,
@@ -77,7 +79,7 @@ app.get("/get-user", (req, res, next) => {
         message: 'uniqueId should not empty'
       });
     }
-    let sql = `SELECT *
+    let sql = `SELECT rowId,name,uniqueId
              FROM users
              WHERE uniqueId  = ?`;
     db.get(sql, [req.query.uniqueId], function (err, row) {
@@ -94,7 +96,8 @@ app.get("/get-user", (req, res, next) => {
         });
       } else {
         //var payload = { 'name': name, 'imageUrl': imageUrl, 'uniqueId': uniqueId };
-        var token = jwt.sign(row, app.get('superSecret'), { expiresIn: '2h' });
+        //var token = jwt.sign(row, app.get('superSecret'), { expiresIn: '2h' });
+        let token = row.name+"-"+row.uniqueId+"-"+row.rowid;
         return res.json({
           success: true,
           token,
@@ -111,4 +114,135 @@ app.get("/get-user", (req, res, next) => {
 
   }
 });
+// submit new score
+app.post('/submit-score',(req,res,next)=>{
+  try {
+    //console.log(req.body);
+    if (req.body.leaderboardName == "" || req.body.leaderboardName == undefined) {
+      return res.json({
+        success: false,
+        message: 'leaderboardName should not empty'
+      });
+    }
+    if (req.body.score == "" || req.body.score == undefined) {
+      return res.json({
+        success: false,
+        message: 'score should not empty'
+      });
+    }
+    if (req.body.token == "" || req.body.token == undefined) {
+      return res.json({
+        success: false,
+        message: 'token should not empty'
+      });
+    }
+    let sql = `SELECT *
+             FROM scores
+             WHERE leaderboardName  = ?`;
+    db.all(sql,[req.body.leaderboardName],function(err,row){
+      if(err){
+        return res.json({
+          success: false,
+          message: err.message
+        });
+      }else{
+        let userUniqueId = req.body.token.split("-");
+        let newRow = {
+          leaderboardName:req.body.leaderboardName,
+          score:req.body.score,
+          place:0,
+          userUniqueId:userUniqueId[1],
+          createdAt:Date.now(),
+          percentage:0
+        }
+        row.forEach((data,index)=>{ //remove existing user data if exist
+          if(data.userUniqueId == newRow.userUniqueId){
+            row.splice(index,1);
+          }
+        })
+        row.push(newRow); //push coming body in data row
+        let userCount = row.length;
+        row.sort(customSortController.customSort); //sort row by score
+        row.forEach((data,index)=>{  //update place and percentage
+          if(index>0 && row[index].score == row[index-1].score){ // if two or more user have same score, then place should be same
+            row[index].place = row[index-1].place;
+            row[index].percentage = parseInt(row[index].place)/userCount;
+          }else{
+            row[index].place = index+1;
+            row[index].percentage = parseInt(row[index].place)/userCount;
+          }
+          if(data.userUniqueId == newRow.userUniqueId){ // update newRow place and percentage for return data
+            newRow.place = row[index].place;
+            newRow.percentage = row[index].percentage;
+          }
+        });
 
+        let deleteSql = "delete from scores where leaderboardName =?";
+        db.run(deleteSql,req.body.leaderboardName,function(err,data){
+          if(err){
+            return res.json({
+              success: false,
+              message: err.message
+            });
+          }else{ //insert updated data in scores table
+            let insertQuery = 'INSERT INTO scores(leaderboardName, score, place, userUniqueId, createdAt, percentage) VALUES(?, ?, ?, ?, ?, ?)';
+            let statement = db.prepare(insertQuery);
+            row.forEach(rowData=>{
+              statement.run([rowData.leaderboardName, rowData.score, rowData.place, rowData.userUniqueId, rowData.createdAt, rowData.percentage],function(err){
+                if (err) {
+                  return res.json({
+                    success: false,
+                    message: err.message
+                  });
+                }
+              });
+            })
+            statement.finalize();
+            return res.json({
+              success: true,
+              place: newRow.place,
+              percentage:newRow.percentage
+            });
+
+          }
+        })
+      }
+    })         
+
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: error.message
+    });
+  }
+});
+
+// get top score of perticular leader board
+app.get('/top-score', (req, res, next) => {
+  try {
+    if(req.query.leaderboardName == "" || req.query.leaderboardName == undefined){
+      return res.json({
+        success: false,
+        message: 'leaderboardName should not empty'
+      });
+    }
+    let sql ="select s.place as rank, s.score, u.name, u.imageUrl, s.createdAt from scores as s INNER JOIN users as u on (s.userUniqueId == u.uniqueId) where s.leaderboardName=?";
+    db.all(sql,[req.query.leaderboardName],function(err, data){
+      console.log(data);
+      console.log(err);
+      if(err){
+        return res.json({
+          success: false,
+          message: error.message
+        });
+      }else{
+        return res.json(data);
+      }
+    })
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: error.message
+    });
+  }
+});
